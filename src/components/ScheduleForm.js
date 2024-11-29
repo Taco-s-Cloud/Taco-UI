0051523059037197fb8ab25fd8c48650624ad7ee
tomasfiure <123346779+tomasfiure@users.noreply.github.com>
2024-11-29 13:10:59 -0500

    Update ScheduleForm.js

--- src/components/ScheduleForm.js
+++ src/components/ScheduleForm.js
@@ -25,7 +25,7 @@ function ScheduleForm({ loadSchedules }) {
     e.preventDefault();
     const { reminderEnabled, ...data } = formData;
     try {
-      await fetch('https://schedule-manager-gcp-1024364663505.us-central1.run.app/schedules', {
+      await fetch('https://schedule-manager-1024364663505.us-central1.run.app/schedules', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify(data)
