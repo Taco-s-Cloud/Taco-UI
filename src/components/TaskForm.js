41253dccae6ddfb49d7cd2bdec29f803427c7709
tomasfiure <123346779+tomasfiure@users.noreply.github.com>
2024-12-08 15:31:01 -0500

    Update TaskForm.js

--- src/components/TaskForm.js
+++ src/components/TaskForm.js
@@ -26,6 +26,13 @@ const TaskForm = () => {
         },
         body: JSON.stringify(formData),
       });
+      // const response = await fetch('http://localhost:5003/tasks', {
+      //   method: 'POST',
+      //   headers: {
+      //     'Content-Type': 'application/json',
+      //   },
+      //   body: JSON.stringify(formData),
+      // });
 
       if (response.ok) {
         alert('Task added successfully!');
