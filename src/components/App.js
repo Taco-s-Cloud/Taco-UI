2e51ea9e7f09f20491dcd6a17993187336e434a5
Lahamgil <134572627+Lahamgil@users.noreply.github.com>
2024-12-10 21:59:34 -0500

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -6,20 +6,22 @@ import UserProfile from './UserProfile';
 import TaskForm from "./TaskForm";
 import ToDoList from './ToDoList';
 import WelcomePage from './WelcomePage';
+import PrintToken from './PrintToken';
 
-
-function App(){
+function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<UserProfile />} />
-        <Route path="/welcome" element={<WelcomePage />} />
-        <Route path="/schedule-manager" element={<ScheduleManager />} />
-        <Route path="/add-schedule" element={<ScheduleForm />} />
-        <Route path="/add-task" element={<TaskForm />} />
-        <Route path="/to-do-list" element={<ToDoList />} />
-      </Routes>
-    </Router>
+      <Router>
+        {/* This will attempt to print the token whenever the user is logged in */}
+        <PrintToken />
+        <Routes>
+          <Route path="/" element={<UserProfile />} />
+          <Route path="/welcome" element={<WelcomePage />} />
+          <Route path="/schedule-manager" element={<ScheduleManager />} />
+          <Route path="/add-schedule" element={<ScheduleForm />} />
+          <Route path="/add-task" element={<TaskForm />} />
+          <Route path="/to-do-list" element={<ToDoList />} />
+        </Routes>
+      </Router>
   );
 }
 
