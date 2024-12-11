6e8800c3ce8f9f69fbe787ff147f5fe6470dbd53
tomasfiure <123346779+tomasfiure@users.noreply.github.com>
2024-12-10 21:55:58 -0500

    Update ScheduleForm.js

--- src/components/ScheduleForm.js
+++ src/components/ScheduleForm.js
@@ -29,7 +29,7 @@ function ScheduleForm({ loadSchedules }) {
     try {
       await makeApiCall('https://schedule-manager-1024364663505.us-central1.run.app/schedules', 'POST', data); // Use the API helper for the request
       //await makeApiCall('http://localhost:5002/schedules', 'POST', data);
-      loadSchedules(); // Refresh the schedules list
+      //loadSchedules(); // Refresh the schedules list
       setFormData({
         title: '',
         description: '',
