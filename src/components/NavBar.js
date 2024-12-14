86f81263b7fc491433e60391cbbb876d7f27285f
ShirShachar <98283083+ShirShachar@users.noreply.github.com>
2024-12-14 11:20:27 -0500

    Update NavBar.js

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,14 +1,21 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import '../styles/NavBar.css';
 
 const NavBar = () => {
   return (
-    <nav>
-      <ul>
-        <li><Link to="/add-schedule">Add Schedule Event</Link></li>
-        <li><Link to="/add-task">Add Task</Link></li>
-        <li><Link to="/todo-list">To Do List</Link></li>
-      </ul>
+    <nav className="navbar">
+      <div className="nav-container">
+        <button className="back-button" onClick={() => window.history.back()}>
+          ← Back
+        </button>
+        <ul className="nav-list">
+          <li><Link to="/schedule-manager" className="nav-link">View Calendar</Link></li>
+          <li><Link to="/add-schedule" className="nav-link">Add Schedule Event</Link></li>
+          <li><Link to="/add-task" className="nav-link">Add Task</Link></li>
+          <li><Link to="/to-do-list" className="nav-link">To-Do List</Link></li>
+        </ul>
+      </div>
     </nav>
   );
 };
